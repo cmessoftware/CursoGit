32eb4eb2e34b4eb41d76eedaf3332f7e0a518993
CMES Software (by Sergio Salanitri) <101049943+cmessoftware@users.noreply.github.com>
2023-09-01 20:14:08 -0300

    Update Calculator.cs

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -21,5 +21,25 @@ public class Calculator : ICalculator
     {
         throw new NotImplementedException();
     }
+
+     public double AddMod(double a, double y, int mod)
+     {
+          throw new NotImplementedException(); 
+     }
+
+     public double MultMod(double x, double y, int mod)
+     {
+         throw new NotImplementedException(); 
+     }
+
+     public double DivMod(double x, double y, int mod)
+     {
+         throw new NotImplementedException(); 
+     }
+
+     public double AbsMod(double x, int module, int mod)
+     {
+         throw new NotImplementedException(); 
+     }
 }
 
