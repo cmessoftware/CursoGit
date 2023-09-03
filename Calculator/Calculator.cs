5cae59b9d7fdc7995d5a82070a1d7f0f581fafcb
SERGIO SALANITRI <ssalanitri@user.noreply.github.com>
2023-09-03 16:55:37 -0300

    Agrego metodos a desarrollar

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -7,7 +7,17 @@ public class Calculator : ICalculator
         throw new NotImplementedException();
     }
 
+    public double AbsMod(double x, int module, int mod)
+    {
+        throw new NotImplementedException();
+    }
+
     public double Add(double x, double y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double AddMod(double a, double y, int mod)
     {
         throw new NotImplementedException();
     }
@@ -17,7 +27,17 @@ public class Calculator : ICalculator
         throw new NotImplementedException();
     }
 
+    public double DivMod(double x, double y, int mod)
+    {
+        throw new NotImplementedException();
+    }
+
     public double Mult(double x, double y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double MultMod(double x, double y, int mod)
     {
         throw new NotImplementedException();
     }
--- Calculator/ICalculator.cs
+++ Calculator/ICalculator.cs
@@ -14,4 +14,12 @@ public interface ICalculator
 
      double Abs(double x, int module);
 
+     double AddMod(double a, double y, int mod);
+
+     double MultMod(double x, double y, int mod);
+
+     double DivMod(double x, double y, int mod);
+
+     double AbsMod(double x, int module, int mod);
+
 }
